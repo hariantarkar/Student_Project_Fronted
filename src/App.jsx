1ec263a5b2c476da696282fa47a04a94a171d5f5
ganeshjadhav9361 <ganeshsjadhav3112@gmail.com>
2025-08-12 12:46:27 +0530

    Add file with logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./HomeComponents/Home.jsx";
 import CourseComponent from "./CourseComponents/CourseComponent.jsx";
-<<<<<<< Updated upstream
+
 import ViewCourses from "./CourseComponents/ViewCourses.jsx";
 import StudentComponent from "./StudentsComponent/studentComponent.jsx";
 import AddNewStudent from "./StudentsComponent/AddNewStudent.jsx";
 import ViewAllStudent from "./StudentsComponent/ViewAllStudent.jsx"; 
 
 
-=======
+
 import Login from "./LoginComponents/Login.jsx";
 import Register from "./LoginComponents/Register.jsx";
 import AdminDashboard from "./DashboardComponent/AdminDashboard.jsx";
->>>>>>> Stashed changes
+
 export default class App extends React.Component {
   render() {
     return (
@@ -23,12 +23,12 @@ export default class App extends React.Component {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/addCourse" element={<CourseComponent />} />
-<<<<<<< Updated upstream
+
             <Route path="/viewCourses" element={<ViewCourses />} />
             <Route path="/studentsData" element={<StudentComponent />} />
             <Route path="/addNewStudent" element={<AddNewStudent />} />
             <Route path="/viewAllStudents" element={<ViewAllStudent />} />
-=======
+
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route
@@ -39,7 +39,6 @@ export default class App extends React.Component {
       : <Home />
   }
 />
->>>>>>> Stashed changes
 
           </Routes>
         </BrowserRouter>
